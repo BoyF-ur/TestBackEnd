8196f54b9d227c9a4aa602b20d8aa5ab3c2e623b
hieppotato <hieppotato@gmail.com>
2025-02-14 11:13:33 +0700

    thêm edit, delete, search book

--- models/travelStory.model.js
+++ models/travelStory.model.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-function removeAccents(str) {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-}
+
 
 const travelStorySchema = new Schema({
     title: { type:String, required: true},
@@ -16,12 +14,6 @@ const travelStorySchema = new Schema({
     createdOn: { type: Date, default: Date.now},
     imageUrl: { type:String, required: true},
     visitedDate: { type:Date, required: true},
-});
-
-travelStorySchema.pre("save", function (next) {
-    this.titleNoDiacritics = removeAccents(this.title);
-    this.storyNoDiacritics = removeAccents(this.story);
-    next();
 });
 
 module.exports = mongoose.model("TravelStory", travelStorySchema);
